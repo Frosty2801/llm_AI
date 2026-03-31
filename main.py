b81eeb5ee47feabd29c8e28d735b0d33426f24a3
camilo-villada <villajuan163@gmail.com>
2026-03-31 13:55:12 -0500

    intelligence chat

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+record = []
+
+while True:
+    user_input = input("You: ")
+
+    record.append(HumanMessage(content=user_input))
+
+    response = llm.invoke(record)
+
+    record.append(AIMessage(content=response.content))
+    print("AI: ", response.content)
+
 
 llm = ChatOpenAI(
     api_key=os.getenv("DEEPSEEK_API_KEY"),
@@ -13,12 +27,15 @@ llm = ChatOpenAI(
 )
 
 prompt = ChatPromptTemplate.from_template(
-    "Explicame {tema} de forma sencilla"
+    """
+    Actua como un asistente financiero experto en inversiones
+        
+    """
 )
 
 chain = prompt | llm
 
-response = chain.invoke({"tema": "modelos llm"})
+response = chain.invoke({"tema": "tensorflow y pytorch y que diferencias hay entre ellos junto con langchain"})
 
 print(response.content)
 
